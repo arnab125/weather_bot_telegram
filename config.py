071d7794c91f9bd5256fe157e51da2f7ed27e1d5
arnab125 <arnabtasnimpranto@gmail.com>
2024-03-18 19:06:59 +0600

    req.txt added

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import dotenv_values
 
-"""
+
 weather_api_key = os.getenv("WEATHER_API_KEY")
 telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-"""
 
+"""
 env = dotenv_values(".env")
 weather_api_key = env["WEATHER_API_KEY"]
 telegram_bot_token = env["TELEGRAM_BOT_TOKEN"]
+"""
